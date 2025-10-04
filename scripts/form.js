76a85147ba409feb406d44cf249cd45d14db4ac0
Sihle-eng <smchobokazi@byupathway.edu>
2025-10-04 17:51:21 +0200

    week05

---
+++ scripts/form.js
@@ -0,0 +1,63 @@
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+const selectElement = document.getElementById("productSelector");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    option.textContent += ` (Average Rating: ${product.averagerating})`;
+
+    selectElement.appendChild(option);
+});
+
+const reviewCountDisplay = document.getElementById("reviewCount");
+if (reviewCountDisplay) {
+    let count = localStorage.getItem("reviewCount");
+    count = count ? parseInt(count) + 1 : 1;
+    localStorage.setItem("reviewCount", count);
+    reviewCountDisplay.textContent = count;
+}
+
+// const selectElemen = document.getElementById("productSelector");
+
+// if (selectElemen) {
+//     products.forEach(product => {
+//         const option = document.createElement("option");
+//         option.value = product.id;
+//         option.textContent = product.name;
+//         option.textContent += ` (Average Rating: ${product.averagerating})`;
+//         selectElemen.appendChild(option);
+//     });
+// }
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = ` ©${currentYear} | ✌️ Sihlelelweyinkhosi Mchobokazi ✌️ | Eswatini`;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`
